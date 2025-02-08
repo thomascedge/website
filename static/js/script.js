d454a449fc0bddecf72a367d16b830ebdd1980d0
Thomas <suavetomas@gmail.com>
2025-02-07 19:12:49 -0600

    included videos, need more fixes

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,25 @@
 
 var hovered_words = $("pic-show"),
     picture_show = $("picture-show"),
-    // meme = $("<video autoplay><source src='assets/videos/cats.mov' type='video/mov'></video>"),
-    // cat = $("<video autoplay muted><source src='assets/videos/cats.mov' type='video/mov'></video>")
-    meme = $("<img src='assets/images/meme0.JPG' alt='meme'>"),
-    cat = $("<img src='assets/images/cats0.HEIC' alt='cats'>");
+    meme = $("<video width='20%' autoplay><source src='assets/videos/meme1.mp4' type='video/mp4'></video>"),
+    cat = $("<video width='20%' autoplay muted><source src='assets/videos/cats.mp4' type='video/mp4'></video>");
 
 hovered_words.mouseenter(function (event) {
     var target = $(event.target)
 
-    if(target.is("meme"))  {
+    if(target.is("#meme"))  {
         picture_show.replaceWith(meme);
-    } if(target.is("cat")) {
-        picture_show.replaceWith(cat)
+    } if(target.is("#cat")) {
+        picture_show.replaceWith(cat);
     }
 });
 
 hovered_words.mouseleave(function (event) {
     var target = $(event.target)
 
-    if(target.is("meme"))  {
+    if(target.is("#meme"))  {
         meme.replaceWith(picture_show);
-    } if(target.is("cat")) {
-        cat.replaceWith(picture_show)
+    } if(target.is("#cat")) {
+        cat.replaceWith(picture_show);
     }
 });
